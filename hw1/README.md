c71dd3ba6b1de86d6043c57b75afa61d0a7b784a
YinXuan1223 <leeyinxuan.cs12@nycu.edu.tw>
2026-03-22 10:56:40 +0800

    finish hw1

--- hw1/README.md
+++ hw1/README.md
@@ -4,3 +4,13 @@ NYCU AI Capstone 2026 Spring
 
 Spec: [Google Docs](https://docs.google.com/document/d/12oA9nxh-qc2v_pnOqRRStw95YPJNO1zy7u75n6enTj4/edit)
 
+# How to run the program
+1. Execute the instructions below :
+    ```
+    conda activate habitat
+    python3 bev2front.py
+    ```
+2. You'll see a small window displaying a BEV image.
+3. Use your mouse to click 4 points in either clockwise or counterclockwise order.
+4. Press any key when you finish defining the region, then you will see the projected polygon in the front view image.
+
--- hw1/bev2front.py
+++ hw1/bev2front.py
@@ -21,8 +21,114 @@ class Projection(object):
             Project the top view pixels to the front view pixels.
             :return: New pixels on perspective(front) view image
         """
+        # camera intrinsic parameters
+        w = 512
+        h = 512
+        u0 = w / 2.0
+        v0 = h / 2.0
+        fx = u0 / np.tan(np.deg2rad(fov / 2.0))
+        fy = v0 / np.tan(np.deg2rad(fov / 2.0))
 
-        ### TODO ###
+        # camera extrinsic parameters
+        C_front = np.array([0.0, 1.0, 0.0])
+        C_bev = np.array([0.0, 2.5, 0.0])
+
+        # camera coordinate is obtained from rotating world coordinate by th_BEV around x-axis (in chinese: how world coordinate do to be camera coordinate)
+        th_BEV = np.deg2rad(90)   
+        Rx_BEV = np.array([
+            [1, 0, 0],
+            [0, np.cos(th_BEV), -np.sin(th_BEV)],
+            [0, np.sin(th_BEV),  np.cos(th_BEV)]
+        ], dtype=np.float64)
+        # front camera coordinate is obtained from rotating world coordinate by th_front around x-axis
+        th_front = np.deg2rad(180)
+        Rx_front = np.array([
+            [1, 0, 0],
+            [0, np.cos(th_front), -np.sin(th_front)],
+            [0, np.sin(th_front),  np.cos(th_front)]
+        ], dtype=np.float64)
+
+        new_pixels = []
+
+        for p in points:
+
+            u, v = p[0], p[1]
+            x_cam = (u - u0) / fx
+            y_cam = (v - v0) / fy
+
+            ray_cam = np.array([x_cam, y_cam, 1.0], dtype=np.float64)
+            ray_world = Rx_BEV @ ray_cam
+
+            lam = -C_bev[1] / ray_world[1]
+            P_world = C_bev + lam * ray_world
+
+            P_cam = np.linalg.inv(Rx_front) @ (P_world - C_front)
+            X, Y, Z = P_cam
+            u_front = fx * (X / Z) + u0
+            v_front = fy * (Y / Z) + v0
+
+            new_pixels.append([int(u_front), int(v_front)])
+
+        return new_pixels
+
+
+    def top_to_front_all_matrices(self, theta=0, phi=0, gamma=0, dx=0, dy=0, dz=0, fov=90):
+        """
+            Project the top view pixels to the front view pixels.
+            :return: New pixels on perspective(front) view image
+        """     
+        # camera intrinsic parameters
+        w = 512
+        h = 512
+        u0 = w / 2.0
+        v0 = h / 2.0
+        fx = u0 / np.tan(np.deg2rad(fov / 2.0))
+        fy = v0 / np.tan(np.deg2rad(fov / 2.0))
+
+        # camera extrinsic parameters
+        C_front = np.array([0.0, 1.0, 0.0])
+        C_bev = np.array([0.0, 2.5, 0.0])
+
+        # camera intrinsic matrix
+        InMat = np.array([
+            [fx, 0, u0],
+            [0, fy, v0],
+            [0, 0,   1]
+        ], dtype=np.float64)
+
+        # camera extrinsic matrices
+        ExtMat_BEV = np.array([
+            [1, 0, 0, 0],
+            [0, 0, 1, 0],
+            [0, -1, 0, 2.5],
+            [0, 0, 0, 1]
+        ], dtype=np.float64)
+
+        ExtMat_front = np.array([
+            [1, 0, 0, 0],
+            [0, -1, 0, 1],
+            [0, 0, -1, 0]
+        ], dtype=np.float64)
+
+        new_pixels = []
+
+        for p in points:
+
+            u, v = p[0], p[1]
+
+            Z_BEVcam = 2.5
+            X_BEVcam = (u-u0)*2.5/fx
+            Y_BEVcam = (v-v0)*2.5/fy
+            P_BEVcam = np.array([X_BEVcam, Y_BEVcam, Z_BEVcam, 1.0])
+
+            P_world = np.linalg.inv(ExtMat_BEV) @ P_BEVcam
+            front_img_homo = InMat @ ExtMat_front @ P_world
+
+            u_front = front_img_homo[0]/front_img_homo[2]
+            v_front = front_img_homo[1]/front_img_homo[2]
+
+            new_pixels.append([int(u_front), int(v_front)])
+
         return new_pixels
 
     def show_image(self, new_pixels, img_name='projection.png', color=(0, 0, 255), alpha=0.4):
